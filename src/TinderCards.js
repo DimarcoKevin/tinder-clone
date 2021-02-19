51c4e07c64558942876e946233fabcf9ef21d4e3
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-02-19 00:33:02 -0500

    importing tinder card and creating two temporary tinder card names and image urls

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,19 +1,29 @@
 import React, {useState} from 'react';
 import "./TinderCards.css";
+import TinderCard from 'react-tinder-card';
 
 function TinderCards() {
 
     const [people, setPeople] = useState([
         {
-            name: "Elon Musk"
+            name: "Elon Musk",
+            url: "https://static.theceomagazine.net/wp-content/uploads/2018/10/15093202/elon-musk-700x467.jpg"
+        },
+        {
+            name: "Jeff Bezos",
+            url: "https://specials-images.forbesimg.com/imageserve/5f469ea85cc82fc8d6083f05/960x0.jpg?fit=scale"
         }
     ]);
 
     return (
         <div className="tinderCards">
+            <div className="tinderCards__cardContainer">
             {people.map((person) => (
-                <h1>{person.name}</h1>
+                <TinderCard>
+                    
+                </TinderCard>
             ))}
+            </div>    
         </div>
     );
 }
