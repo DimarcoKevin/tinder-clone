371710bb07fa166e0e0dbf35db5f383e67a916f5
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-02-18 21:03:50 -0500

    Adding import for person icon and utilizing component

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import  "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
 
 class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h2>Insert Header Here</h2>
+                <PersonIcon fontSize="large" className="header__icon" />
             </div>
         );
     }
