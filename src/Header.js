0c43088472b01d907bea0a16e0841d01e4a6731f
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-02-18 22:12:03 -0500

    Adding tinder logo

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import  "./Header.css";
+import TinderIcon from './images/tinder-logo.png';
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
 
@@ -10,6 +11,13 @@ class Header extends Component {
                 <IconButton>
                     <PersonIcon fontSize="large" className="header__icon" />
                 </IconButton>
+
+                <img
+                    className="header__logo"
+                    src={TinderIcon}
+                />
+                
+
             </div>
         );
     }
