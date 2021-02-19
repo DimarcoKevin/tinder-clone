768b69287055c6e175176a9b4be19508bce35029
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-02-18 21:05:25 -0500

    Turning personIcon into a buttonIcon

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import  "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
 
 class Header extends Component {
     render() {
         return (
             <div className="header">
-                <PersonIcon fontSize="large" className="header__icon" />
+                <IconButton>
+                    <PersonIcon fontSize="large" className="header__icon" />
+                </IconButton>
             </div>
         );
     }
